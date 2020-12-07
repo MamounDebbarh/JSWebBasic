acfd35cb88c60c424e8663039eb161d971633e1b
MamounDebbarh <m.m.debbarh@gmail.com>
2020-12-07 15:13:58 +0100

    Commit test

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("hello world");
+    console.log("commit test");
   res.render('index', { title: 'Express' });
 });
 
